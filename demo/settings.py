047196cde0b3bbb8e8cce02db3414cc8b94c96f5
sam123 <smaylopco@yahoo.es>
2016-11-23 10:31:17 -0600

    se arreglo la interfaz y se corrigieron algunos errores

--- demo/settings.py
+++ demo/settings.py
@@ -1,5 +1,7 @@
 # Django settings for demo project.
 import os
+
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 DEBUG = True
 TEMPLATE_DEBUG = DEBUG
 
@@ -72,7 +74,7 @@ MEDIA_URL = '/media/'
 # Don't put anything in this directory yourself; store your static files
 # in apps' "static/" subdirectories and in STATICFILES_DIRS.
 # Example: "/home/media/media.lawrence.com/static/"
-STATIC_ROOT = ''
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 # URL prefix for static files.
 # Example: "http://media.lawrence.com/static/"
